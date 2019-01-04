194517ff55596d9750fdc3cf3ca0e518a90bffa1
kirtiag04 <kirtikeeru04@gmail.com>
2019-01-04 12:15:33 -0800

    adding code

---
+++ config.py
@@ -0,0 +1,8 @@
+#!flask/bin/python
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database location
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
